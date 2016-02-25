a18b712eb649ea5caef13d2765dfdb948de0f805
muhnizar <mooh.nizar@yahoo.com>
2016-02-25 21:13:10 +0700

    Initial commit

---
+++ app/src/main/java/com/example/moohn/todos/TodosOverviewActivity.java
@@ -0,0 +1,13 @@
+package com.example.moohn.todos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TodosOverviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_todos_overview);
+    }
+}
