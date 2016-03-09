4ef931bb6897b84d8fe2ffb7fffd4cd395bd39b7
Nizar <mooh.nizar@gmail.com>
2016-03-09 16:19:31 +0700

    > set view list

--- app/src/main/java/com/example/moohn/todos/TodosOverviewActivity.java
+++ app/src/main/java/com/example/moohn/todos/TodosOverviewActivity.java
@@ -1,13 +1,33 @@
 package com.example.moohn.todos;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ListActivity;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
 
-public class TodosOverviewActivity extends AppCompatActivity {
+public class TodosOverviewActivity extends ListActivity implements LoaderManager.LoaderCallbacks<Cursor> {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_todos_overview);
+        setContentView(R.layout.todo_list);
+        registerForContextMenu(getListView());
+    }
+
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+
     }
 }
