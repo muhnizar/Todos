b652ef6a0552fd6e4a3527aeb52887832f7579cc
muhnizar <mooh.nizar@yahoo.com>
2016-03-11 04:53:26 +0700

    >start activity for detail

--- app/src/main/java/com/example/moohn/todos/TodosOverviewActivity.java
+++ app/src/main/java/com/example/moohn/todos/TodosOverviewActivity.java
@@ -2,10 +2,14 @@ package com.example.moohn.todos;
 
 import android.app.ListActivity;
 import android.content.CursorLoader;
+import android.content.Intent;
 import android.content.Loader;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.app.LoaderManager;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.SimpleCursorAdapter;
 
 import com.example.moohn.todos.com.example.moohn.todos.contentprovider.TodoContentProvider;
@@ -14,6 +18,7 @@ import com.example.moohn.todos.database.TodoTable;
 public class TodosOverviewActivity extends ListActivity implements LoaderManager.LoaderCallbacks<Cursor> {
 
     private SimpleCursorAdapter adapter;
+    private static final int DELETE_ID = Menu.FIRST + 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,8 +39,34 @@ public class TodosOverviewActivity extends ListActivity implements LoaderManager
         setListAdapter(adapter  );
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.listmenu, menu);
+        return true;
+    }
 
-//  immediately called after init loader
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.insert:
+            createTodo();    
+            return true;
+            
+        
+        
+        }
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void createTodo() {
+        Intent i = new Intent(this, TodoDetailActivity.class);
+        startActivity(i);
+    }
+
+    //  immediately called after init loader
     @Override
     public Loader<Cursor> onCreateLoader(int id, Bundle args) {
         String[] projection =  {TodoTable.COLUMN_ID, TodoTable.COLUMN_SUMMARY};
