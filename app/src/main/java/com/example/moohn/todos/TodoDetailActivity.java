65b0195b7fe75723d433a1dc7091a3114e68c3a0
muhnizar <mooh.nizar@yahoo.com>
2016-03-11 06:17:14 +0700

    >on create associate with view edit

--- app/src/main/java/com/example/moohn/todos/TodoDetailActivity.java
+++ app/src/main/java/com/example/moohn/todos/TodoDetailActivity.java
@@ -1,4 +1,27 @@
 package com.example.moohn.todos;
 
-public class TodoDetailActivity {
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+public class TodoDetailActivity extends Activity{
+    private Spinner mCategory;
+    private EditText mSummary;
+    private EditText mDescription;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.todo_edit);
+        
+        mCategory =(Spinner) findViewById(R.id.category);
+        mSummary = (EditText) findViewById(R.id.todo_edit_summary);
+        mDescription = (EditText) findViewById(R.id.todo_edit_description);
+        Button confirmButton = (Button) findViewById(R.id.todo_edit_button);
+
+        Bundle extra = getIntent().getExtras();
+
+    }
 }
