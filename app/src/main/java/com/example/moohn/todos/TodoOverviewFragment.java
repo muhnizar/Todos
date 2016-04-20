3b8c8c22dd4252da7942e5441656d1026fca5426
muhnizar <mooh.nizar@yahoo.com>
2016-04-21 05:41:50 +0700

    >fix bugs finding tag fragment

--- app/src/main/java/com/example/moohn/todos/TodoOverviewFragment.java
+++ app/src/main/java/com/example/moohn/todos/TodoOverviewFragment.java
@@ -2,6 +2,7 @@ package com.example.moohn.todos;
 
 import android.app.ListFragment;
 import android.app.LoaderManager;
+import android.content.CursorLoader;
 import android.content.Loader;
 import android.database.Cursor;
 import android.os.Bundle;
@@ -9,34 +10,42 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.moohn.todos.adapter.TodoCursorAdapter;
+import com.example.moohn.todos.com.example.moohn.todos.contentprovider.TodoContentProvider;
+import com.example.moohn.todos.database.TodoTable;
+
 public class TodoOverviewFragment extends ListFragment  implements LoaderManager.LoaderCallbacks<Cursor>{
+
+    private TodoCursorAdapter todoAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        return inflater.inflate(R.layout.todo_list_fragment,container);
     }
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-
-
+        getLoaderManager().initLoader(0, null, this);
     }
 
 
 
     @Override
     public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-        return null;
+        String[] projection =  {TodoTable.COLUMN_ID, TodoTable.COLUMN_SUMMARY, TodoTable.COLUMN_CATEGORY};
+        return new CursorLoader(getActivity(), TodoContentProvider.CONTENT_URI,projection,null,null,null);
     }
 
     @Override
     public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
-
+        todoAdapter = new TodoCursorAdapter(getActivity(), data);
+        setListAdapter(todoAdapter);
+        todoAdapter.swapCursor(data);
     }
 
     @Override
     public void onLoaderReset(Loader<Cursor> loader) {
-
+        todoAdapter.swapCursor(null);
     }
 }
--- app/src/main/java/com/example/moohn/todos/TodosOverviewActivity.java
+++ app/src/main/java/com/example/moohn/todos/TodosOverviewActivity.java
@@ -1,25 +1,12 @@
 package com.example.moohn.todos;
 
 import android.app.FragmentManager;
-import android.content.CursorLoader;
-import android.content.Intent;
-import android.content.Loader;
-import android.database.Cursor;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.view.ContextMenu;
 import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 
 import com.example.moohn.todos.adapter.TodoCursorAdapter;
-import com.example.moohn.todos.com.example.moohn.todos.contentprovider.TodoContentProvider;
-import com.example.moohn.todos.database.TodoTable;
 
 public class TodosOverviewActivity extends FragmentActivity {
 
@@ -30,98 +17,14 @@ public class TodosOverviewActivity extends FragmentActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.todo_list);
-        fillData();
 
+        setContentView(R.layout.todo_list);
         FragmentManager fm = getFragmentManager();
         // Create the list fragment and add it as our sole content.
-        if (fm.findFragmentById(android.R.id.content) == null) {
-            CursorLoaderListFragment listFragment = new CursorLoaderListFragment();
-            fm.beginTransaction().add(android.R.id.content, list).commit();
+        if (fm.findFragmentById(R.id.todoListFragment) == null) {
+            TodoOverviewFragment listFragment = new TodoOverviewFragment();
+            fm.beginTransaction().add(R.id.todoListFragment , listFragment).commit();
         }
-
-//        register context menu to the list
-        registerForContextMenu(getListView());
     }
 
-    private void fillData() {
-//      init loader
-        getLoaderManager().initLoader(0, null, this);
-
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu){
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.listmenu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
-            case R.id.insert:
-            createTodo();
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
-        super.onCreateContextMenu(menu, v, menuInfo);
-        menu.add(0, DELETE_ID, 0,R.string.menu_delete);
-    }
-
-    @Override
-    public boolean onContextItemSelected(MenuItem item) {
-        switch (item.getItemId()){
-            case DELETE_ID:
-                AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
-                Uri uri = Uri.parse(TodoContentProvider.CONTENT_URI +"/"+info.id);
-                getContentResolver().delete(uri,null,null);
-                fillData();
-            return true;
-        }
-
-        return super.onContextItemSelected(item);
-    }
-
-    private void createTodo() {
-        Intent i = new Intent(this, TodoDetailActivity.class);
-        startActivity(i);
-    }
-
-    //  immediately called after init loader
-    @Override
-    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-        String[] projection =  {TodoTable.COLUMN_ID, TodoTable.COLUMN_SUMMARY, TodoTable.COLUMN_CATEGORY};
-
-        return new CursorLoader(this, TodoContentProvider.CONTENT_URI, projection, null, null, null);
-    }
-
-    @Override
-    protected void onListItemClick(ListView l, View v, int position, long id) {
-        super.onListItemClick(l, v, position, id);
-        Intent i = new Intent(this, TodoDetailActivity.class);
-        Uri todoUri = Uri.parse(TodoContentProvider.CONTENT_URI +"/"+ id);
-        i.putExtra(TodoContentProvider.CONTENT__ITEM_TYPE,todoUri );
-        startActivity(i);
-    }
-
-    @Override
-    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
-//    the asynchronous load is complete and data is available now.
-//    now we can associate with adapter
-        todoAdapter = new TodoCursorAdapter(this, data);
-        setListAdapter(todoAdapter);
-        todoAdapter.swapCursor(data);
-    }
-
-    @Override
-    public void onLoaderReset(Loader<Cursor> loader) {
-//      data is now unavailable;
-        adapter.swapCursor(null);
-    }
 }
