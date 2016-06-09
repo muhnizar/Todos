e436523ad32ca82080a606c6e2ba564f51cd372c
muhnizar <mooh.nizar@yahoo.com>
2016-06-10 06:42:56 +0700

    +add view pager +add Coordinator layout  for tab

--- app/src/main/java/com/example/moohn/todos/TodosOverviewActivity.java
+++ app/src/main/java/com/example/moohn/todos/TodosOverviewActivity.java
@@ -2,11 +2,16 @@ package com.example.moohn.todos;
 
 import android.app.FragmentManager;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.view.Menu;
 import android.widget.SimpleCursorAdapter;
 
 import com.example.moohn.todos.adapter.TodoCursorAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TodosOverviewActivity extends FragmentActivity {
 
@@ -24,6 +29,26 @@ public class TodosOverviewActivity extends FragmentActivity {
         if (fm.findFragmentById(R.id.todoListFragment) == null) {
             TodoOverviewFragment listFragment = new TodoOverviewFragment();
             fm.beginTransaction().add(R.id.todoListFragment , listFragment).commit();
+        }
+    }
+
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(android.support.v4.app.FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return 0;
         }
     }
 
