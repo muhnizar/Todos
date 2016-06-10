3e8afb0be6f48094eba41877625bdae562a613a0
Nizar <Nizar@Nizar-iMac-971.local>
2016-06-10 15:53:29 +0700

    add view pager

--- app/src/main/java/com/example/moohn/todos/TodosOverviewActivity.java
+++ app/src/main/java/com/example/moohn/todos/TodosOverviewActivity.java
@@ -1,10 +1,12 @@
 package com.example.moohn.todos;
 
-import android.app.FragmentManager;
+import android.support.v4.app.FragmentManager;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.Menu;
 import android.widget.SimpleCursorAdapter;
 
@@ -24,7 +26,7 @@ public class TodosOverviewActivity extends FragmentActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.todo_list);
-        FragmentManager fm = getFragmentManager();
+        FragmentManager fm = getSupportFragmentManager();
         // Create the list fragment and add it as our sole content.
         if (fm.findFragmentById(R.id.todoListFragment) == null) {
             TodoOverviewFragment listFragment = new TodoOverviewFragment();
@@ -32,23 +34,45 @@ public class TodosOverviewActivity extends FragmentActivity {
         }
     }
 
+    private  void setupViewPager(ViewPager viewPager){
+        FragmentManager fm = getSupportFragmentManager();
+        TodoOverviewFragment listFragment = null;
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(fm);
+        if (fm.findFragmentById(R.id.todoListFragment) == null) {
+            listFragment = new TodoOverviewFragment();
+            fm.beginTransaction().add(R.id.todoListFragment , listFragment).commit();
+        }
+        viewPagerAdapter.addFragment(listFragment, "list");
+    }
+
+
     class ViewPagerAdapter extends FragmentPagerAdapter {
 
         private final List<Fragment> mFragmentList = new ArrayList<>();
         private final List<String> mFragmentTitleList = new ArrayList<>();
 
-        public ViewPagerAdapter(android.support.v4.app.FragmentManager fm) {
-            super(fm);
+        public ViewPagerAdapter(FragmentManager fm) {
+            super(getSupportFragmentManager());
+        }
+
+        public void addFragment(Fragment fragment, String title){
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
         }
 
         @Override
         public Fragment getItem(int position) {
-            return null;
+            return mFragmentList.get(position);
         }
 
         @Override
         public int getCount() {
-            return 0;
+            return mFragmentTitleList.size();
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
         }
     }
 
